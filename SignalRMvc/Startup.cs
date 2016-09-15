3938aa6b97a2878a3638d6c4a5b96b3312fa81ff
PavloZalevsky <pavlo.zalevsky@gmail.com>
2016-09-15 22:29:52 +0300

    Add project files.

---
+++ SignalRMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRMvc.Startup))]
+namespace SignalRMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
